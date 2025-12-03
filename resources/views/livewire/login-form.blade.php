1ad1c81bed6ee334669bba0cb82e66d776d91d56
jxrdel <jrdlregis@gmail.com>
2025-12-03 17:43:57 -0400

    Spinner on login page

--- resources/views/livewire/login-form.blade.php
+++ resources/views/livewire/login-form.blade.php
@@ -1,18 +1,28 @@
 <div>
-    <div style="color:red; text-align: center;padding:10px">@error('error') {{ $message }} @enderror</div>
+    <div style="color:red; text-align: center;padding:10px">
+        @error('error')
+            {{ $message }}
+        @enderror
+    </div>
     <form class="user" wire:submit.prevent="login">
         <div class="form-group">
-            <input type="username" class="form-control form-control-user @error('username')is-invalid @enderror" id="username" wire:model="username" autocomplete="off"
-                id="exampleInputusername" aria-describedby="usernameHelp" style="font-size: 1rem"
-                placeholder="firstname.lastname">
+            <input type="username" class="form-control form-control-user @error('username')is-invalid @enderror"
+                id="username" wire:model="username" autocomplete="off" id="exampleInputusername"
+                aria-describedby="usernameHelp" style="font-size: 1rem" placeholder="firstname.lastname">
         </div>
         <div class="form-group">
-            <input type="password" class="form-control form-control-user @error('password')is-invalid @enderror" wire:model="password"
-                id="exampleInputPassword" placeholder="Password"style="font-size: 1rem">
-                <div style="color:red; text-align: center;padding:10px">@error('password') {{ $message }} @enderror</div>
+            <input type="password" class="form-control form-control-user @error('password')is-invalid @enderror"
+                wire:model="password" id="exampleInputPassword" placeholder="Password"style="font-size: 1rem">
+            <div style="color:red; text-align: center;padding:10px">
+                @error('password')
+                    {{ $message }}
+                @enderror
+            </div>
         </div>
         <button class="btn btn-primary btn-user btn-block" style="font-size: 1rem">
             Login
+            <span wire:loading wire:target="login" class="spinner-border spinner-border-sm" style="margin-bottom: 3px"
+                role="status" aria-hidden="true"></span>
         </button>
     </form>
 </div>
